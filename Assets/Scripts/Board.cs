6c42bbe7e672a4242f4745af5f3fde5024904cb6
Greg Tsang <11238872+gregtsang@users.noreply.github.com>
2019-07-10 21:46:38 +0800

    Rename variables

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,12 +1,12 @@
 ﻿using UnityEngine;
 
-public class Map : MonoBehaviour
+public class Board : MonoBehaviour
 {
     public Camera gameCam;
     public GameState gameState;
     public GameObject tilePrefab;
 
-    public int mapWidth;
+    public int boardWidth = 20;
 
     float tileWidth = 1.73205f;
     float tileLength = 2.0f;
@@ -14,10 +14,10 @@ public class Map : MonoBehaviour
     
     void Start()
     {
-        gameState.SetCap(mapWidth);
-        gameCam.orthographicSize = mapWidth;
+        gameState.SetCap(boardWidth);
+        gameCam.orthographicSize = boardWidth;
         SetGaps(tileGap);
-        CreateMap();
+        CreateBoard();
     }
 
     void SetGaps(float gap)
@@ -26,27 +26,27 @@ public class Map : MonoBehaviour
         tileLength += tileLength * gap;
     }
 
-    Vector3 CalcPos(Vector2Int mapPos, int numTiles)
+    Vector3 CalcPos(Vector2Int boardPos, int numTiles)
     {
-        float x = (numTiles - 1.0f) * -0.75f * tileLength + mapPos.x * 1.5f * tileLength;
-        float z = (mapWidth - 1.0f - mapPos.y) * (tileWidth / 2.0f);
+        float x = (numTiles - 1.0f) * -0.75f * tileLength + boardPos.x * 1.5f * tileLength;
+        float z = (boardWidth - 1.0f - boardPos.y) * (tileWidth / 2.0f);
 
         return new Vector3(x, 0, z);
     }
 
-    void CreateMap()
+    void CreateBoard()
     {
-        for (int y = 0; y < 2 * mapWidth - 1; y++)
+        for (int y = 0; y < 2 * boardWidth - 1; y++)
         {
-            int numTiles = mapWidth - Mathf.Abs(mapWidth - y - 1);
+            int numTiles = boardWidth - Mathf.Abs(boardWidth - y - 1);
 
             for (int x = 0; x < numTiles; x++)
             {
-                Vector2Int mapPos = new Vector2Int(x, y);
-                Vector3 tilePos = CalcPos(mapPos, numTiles);
+                Vector2Int boardPos = new Vector2Int(x, y);
+                Vector3 tilePos = CalcPos(boardPos, numTiles);
                 GameObject tile = Instantiate(tilePrefab, tilePos, Quaternion.identity, this.transform) as GameObject;
-                int q = y < mapWidth ? mapWidth - 1 - y + x : x;
-                int r = y < mapWidth ? x : y - mapWidth + 1 + x;
+                int q = y < boardWidth ? boardWidth - 1 - y + x : x;
+                int r = y < boardWidth ? x : y - boardWidth + 1 + x;
                 tile.name = "Tile" + q + "." + r;
                 gameState.InitNode(tile, q, r);
                 tile.GetComponent<TilePrefab>().SetCoords(q, r);
