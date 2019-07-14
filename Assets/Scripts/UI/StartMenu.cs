6915a949c8cd883455660f6369b5a7c9f1d40a7b
Miskewicz <miskewie@oregonstate.edu>
2019-07-14 17:35:47 -0400

    Basic Start Menu Scene - Imported TMPro

---
+++ Assets/Scripts/UI/StartMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GeometryBattles.UI
+{ 
+    public class StartMenu : MonoBehaviour
+    {
+        [SerializeField] GameObject startCanvas = null;
+        [SerializeField] GameObject creditsCanvas = null;
+        
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+        
+        public void ShowStartMenuCanvas()
+        {
+            DisableAllCanvases();
+            startCanvas.SetActive(true);
+        }
+        
+        public void ShowCreditsMenuCanvas()
+        {
+            DisableAllCanvases();
+            creditsCanvas.SetActive(true);
+        }
+
+        public void LoadGameScene()
+        {
+            SceneManager.LoadScene(2);
+        }
+
+        private void DisableAllCanvases()
+        {
+            var allCanvases = FindObjectsOfType<Canvas>();
+            foreach (Canvas canvas in allCanvases)
+            {
+                canvas.gameObject.SetActive(false);
+            }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
