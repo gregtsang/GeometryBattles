f94cc3de0bbf390314d94d106e8f424899c1ad43
Miskewicz <miskewie@oregonstate.edu>
2019-07-14 15:04:30 -0400

    Add HUD with basic resource display

---
+++ Assets/Scripts/UI/ResourceDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GeometryBattles.PlayerManager;
+
+namespace GeometryBattles.UI
+{
+    public class ResourceDisplay : MonoBehaviour
+    {
+        private Text text;
+        private UIManager uiManager;
+
+        private void Start()
+        {
+            text = GetComponent<Text>();
+            uiManager = FindObjectOfType<UIManager>();
+        }
+
+        private void Update()
+        {
+            text.text = "Resources: " + uiManager.GetActivePlayer().GetResource().ToString();
+        }
+    }
+}
