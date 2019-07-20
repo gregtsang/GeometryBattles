50055cb800e5cd348e6ecc2d209827a4edf7e8b0
Danny W <seldon.wilson@gmail.com>
2019-07-20 08:57:53 -0700

    Removed warning and added debug log for master client upon board creation.

--- Assets/Scripts/Managers/NetworkedManager.cs
+++ Assets/Scripts/Managers/NetworkedManager.cs
@@ -6,12 +6,13 @@ using UnityEngine;
 public class NetworkedManager : MonoBehaviour
 {
    [SerializeField]
-   private GameObject _boardPrefab;
+   private GameObject _boardPrefab = null;
 
    private void Awake()
    {
       if (PhotonNetwork.IsMasterClient)
       {
+         Debug.Log("I am the master client and I am about to instantiate the board.");
          MasterManager.NetworkInstantiate(_boardPrefab, Vector3.zero, Quaternion.identity);
       }
    }
