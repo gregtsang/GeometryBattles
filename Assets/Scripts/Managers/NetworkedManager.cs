ff196a0eeac4f347aa501e75cfb3ad8062d5e99b
Danny W <seldon.wilson@gmail.com>
2019-07-27 11:37:33 -0700

    Successfully can start networked game w/ new board type.

--- Assets/Scripts/Managers/NetworkedManager.cs
+++ Assets/Scripts/Managers/NetworkedManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Photon.Pun;
+﻿using Photon.Pun;
 using UnityEngine;
 
 public class NetworkedManager : MonoBehaviour
@@ -20,16 +18,4 @@ public class NetworkedManager : MonoBehaviour
          MasterManager.NetworkInstantiate(_boardPrefab, Vector3.zero, Quaternion.identity);
       }
    }
-
-   // Start is called before the first frame update
-   void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
