94fc96e7df83bf36c25358077d94a5d9d9613c6b
Danny W <seldon.wilson@gmail.com>
2019-08-02 14:53:06 -0700

    Chat.cs implements (trivially) the IChatClientListener interface.

--- Assets/Scripts/Chat/Chat.cs
+++ Assets/Scripts/Chat/Chat.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Chat;
+using ExitGames.Client.Photon;
 
-public class Chat : MonoBehaviour
+public class Chat : MonoBehaviour, IChatClientListener
 {
     public string username;
 
@@ -22,4 +24,62 @@ public class Chat : MonoBehaviour
     public void Connect() {
         ;
     }
+
+#region IChatClientListener implementation
+   void IChatClientListener.DebugReturn(DebugLevel level, string message)
+   {
+      throw new System.NotImplementedException();
+   }
+
+   void IChatClientListener.OnDisconnected()
+   {
+      throw new System.NotImplementedException();
+   }
+
+   void IChatClientListener.OnConnected()
+   {
+      throw new System.NotImplementedException();
+   }
+
+   void IChatClientListener.OnChatStateChange(ChatState state)
+   {
+      throw new System.NotImplementedException();
+   }
+
+   void IChatClientListener.OnGetMessages(string channelName, string[] senders, object[] messages)
+   {
+      throw new System.NotImplementedException();
+   }
+
+   void IChatClientListener.OnPrivateMessage(string sender, object message, string channelName)
+   {
+      throw new System.NotImplementedException();
+   }
+
+   void IChatClientListener.OnSubscribed(string[] channels, bool[] results)
+   {
+      throw new System.NotImplementedException();
+   }
+
+   void IChatClientListener.OnUnsubscribed(string[] channels)
+   {
+      throw new System.NotImplementedException();
+   }
+
+   void IChatClientListener.OnStatusUpdate(string user, int status, bool gotMessage, object message)
+   {
+      throw new System.NotImplementedException();
+   }
+
+   void IChatClientListener.OnUserSubscribed(string channel, string user)
+   {
+      throw new System.NotImplementedException();
+   }
+
+   void IChatClientListener.OnUserUnsubscribed(string channel, string user)
+   {
+      throw new System.NotImplementedException();
+   }
+
+   #endregion
 }
