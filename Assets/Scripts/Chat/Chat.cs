89ced405c95da477507e054d56bd5aecd2010364
Danny W <seldon.wilson@gmail.com>
2019-08-03 11:08:08 -0700

    Implemented Chat object's Start and Connect methods, along with clean-up methods OnDestroy or OnApplicationQuit. Connect currently doesn't work becuase all callbacks are not yet defined.

--- Assets/Scripts/Chat/Chat.cs
+++ Assets/Scripts/Chat/Chat.cs
@@ -2,27 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Chat;
+using Photon.Realtime;
+using Photon.Pun;
 using ExitGames.Client.Photon;
 
 public class Chat : MonoBehaviour, IChatClientListener
 {
-    public string username;
+    public string username = "unpicked_username";
+
+    [SerializeField]
+    private uint _historyBufferLength;
+
+    [SerializeField]
+    private string appVersion;
+
+    private ChatClient _chatClient;
+
+    protected internal AppSettings chatAppSettings;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+            // We want to be able to chat in subsequent scenes
+            // TODO - warning on this call 
+        DontDestroyOnLoad(this.gameObject);
+
+            // Determine if the application has an appid for Photon Chat
+        chatAppSettings = PhotonNetwork.PhotonServerSettings.AppSettings;
+        if (chatAppSettings.AppIdChat is null)
+        {
+            Debug.Log("Missing appid for Photon Chat");
+        }
+        else
+        {
+            Debug.Log($"Photon Chat appid: {chatAppSettings.AppIdChat}");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+            // Keep an active connection alive and process incoming messages
+        _chatClient?.Service();
     }
 
-    public void Connect() {
-        ;
+    public void Connect()
+    {
+        _chatClient = new ChatClient(this);
+
+        #if UNITY_WEBGL
+            _chatClient.UseBackgroundWorkerForSending = false;
+        #else
+            _chatClient.UseBackgroundWorkerForSending = true;
+        #endif
+
+        _chatClient.Connect(
+            chatAppSettings.AppIdChat, 
+            appVersion,
+            new Photon.Chat.AuthenticationValues(username)
+        );
+
+        Debug.Log($"Connecting to Photon Chat servers as {username}");
+    }
+
+    private void OnDestroy()
+    {
+        _chatClient?.Disconnect();
+    }
+
+    private void OnApplicationQuit()
+    {
+        _chatClient?.Disconnect();
     }
 
 #region IChatClientListener implementation
