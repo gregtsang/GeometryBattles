8e6be58cf5413449ce8b234aefa5d6312591c6ea
Greg Tsang <11238872+gregtsang@users.noreply.github.com>
2019-08-12 03:01:58 +0800

    Add hexagon animation

--- Assets/Scripts/Structures/HexagonData.cs
+++ Assets/Scripts/Structures/HexagonData.cs
@@ -1,18 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HexagonData : MonoBehaviour
+﻿namespace GeometryBattles.StructureManager
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class HexagonLevel : StructureLevel
     {
-        
+        public float buildTime;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public class HexagonData : StructureData<HexagonLevel> { }
 }
